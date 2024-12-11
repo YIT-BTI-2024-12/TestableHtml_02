6fac5f89983efcd85441b7e259cc4d922d475b9b
김민채 <minchaeg85@gmail.com>
2024-12-11 16:12:30 +0900

    fix :: 상수 선언 추가

--- src/main/java/com/cleancode/fitnesse/testablehtml/TestableHtml.java
+++ src/main/java/com/cleancode/fitnesse/testablehtml/TestableHtml.java
@@ -7,24 +7,27 @@ public class TestableHtml {
 
     public static final String INCLUDE_SETUP = "!include -setup .";
     public static final String INCLUDE_TEARDOWN = "!include -teardown .";
+    public static final String TEST = "Test";
+    public static final String SET_UP = "SetUp";
+    public static final String TEAR_DOWN = "TearDown";
 
     public String testableHtml(PageData pageData, boolean includeSuiteSetup) throws Exception {
         WikiPage wikiPage = pageData.getWikiPage();
         StringBuffer contentBuffer = new StringBuffer();
 
-        boolean checkPageData = pageData.hasAttribute("Test");
+        boolean checkPageData = pageData.hasAttribute(TEST);
 
         if (checkPageData) {
             if (includeSuiteSetup) {
                 renderPage(wikiPage, contentBuffer, INCLUDE_SETUP, SuiteResponder.SUITE_SETUP_NAME);
             }
-            renderPage(wikiPage, contentBuffer, INCLUDE_SETUP, "SetUp");
+            renderPage(wikiPage, contentBuffer, INCLUDE_SETUP, SET_UP);
         }
 
         contentBuffer.append(pageData.getContent());
 
         if (checkPageData) {
-            renderPage(wikiPage, contentBuffer, INCLUDE_TEARDOWN, "TearDown");
+            renderPage(wikiPage, contentBuffer, INCLUDE_TEARDOWN, TEAR_DOWN);
 
             if (includeSuiteSetup) {
                 renderPage(wikiPage, contentBuffer, INCLUDE_TEARDOWN, SuiteResponder.SUITE_TEARDOWN_NAME);
